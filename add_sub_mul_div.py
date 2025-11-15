362b5779c872a706517c876b676e7e1f7c59a9cf
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-15 13:12:48 +0530

    mul

--- add_sub_mul_div.py
+++ add_sub_mul_div.py
@@ -11,3 +11,9 @@ num1 = int(input("enter your first no : "))
 num2 = int(input("enter your decond no "))
 sub = num1 - num2
 print (" the sub is: ",sub)
+
+# multiplication  program.
+num1 = int(input("enter your first number: "))
+num2 = int(input("enter your second number: "))
+mul = num1 * num2
+print ("the multiplication is: ",mul)
