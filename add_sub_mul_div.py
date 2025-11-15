5457e07ba531e6fad7051f155e265c7ba268275f
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-15 13:08:48 +0530

    sub

--- add_sub_mul_div.py
+++ add_sub_mul_div.py
@@ -1,6 +1,13 @@
+# Addition program
 num1 = int(input("enter your first number : "))
 num2 = int(input("enter your second number : "))
 
 # add two number
 sum = num1 + num2
 print("the sum is ",sum)
+
+# Substraction program
+num1 = int(input("enter your first no : "))
+num2 = int(input("enter your decond no "))
+sub = num1 - num2
+print (" the sub is: ",sub)
