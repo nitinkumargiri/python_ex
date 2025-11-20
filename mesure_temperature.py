a60ad40a7b9dadfae68246c74215dba510a8612a
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-20 20:24:33 +0530

    tru

--- mesure_temperature.py
+++ mesure_temperature.py
@@ -3,4 +3,7 @@ temp = float(input("enter your temperature : "))
 mesur = (temp * 5/9)+ 32
 print(f"fahrenheit of {temp} is ",mesur)
 
+#we will again erite program for fahrenheit to celcius.
+fahrenheit = float(input("ente your no : "))
+
 
