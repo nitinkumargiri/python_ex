5dcc28e1d8ebbb6d9b93a89021b8c948f15e2ea3
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-21 08:49:48 +0530

    cal

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
 num1 = int(input("enter your first number : "))
 num2 = int(input("enter your second number : "))
 operator = str(input("entr your choice (+,-,*,/) : "))
+if(operator == '+'):
+    print(f"the sum of {num1} + {num2} = ",num1+num2)
