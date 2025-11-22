5e739a6549d7d035270e25a0c4250e915aa21b46
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-22 09:19:55 +0530

    rock

---
+++ number_guessing_game.py
@@ -0,0 +1,10 @@
+guess = int (input("enter your number : "))
+import random
+computerguess = random.randrange(1,100)
+print("computer guess no is : ",computerguess)
+if(guess < computerguess):
+    print("number too low....")
+elif(guess > computerguess):
+    print("number too heigh...")
+else:
+    print("congratulation: your number is same\nits a lucky number..")
---
+++ rock_paper_scissor.py
@@ -0,0 +1,17 @@
+# import
+print("....WELCOME TO ROCK PAPPER SCISOR GAME....\n\n")
+print("chose: 1 = 'rock'..\nchose: 2 = 'paper'..\nchose: 3 = 'scissor..\n\n")
+
+# userchoice = int (input("enter your choice : "))
+# import random
+# computerchoice = (random.randrange(1,4))
+# print("enter computer choice : ",computerchoice)
+
+# if(userchoice == computerchoice):
+#     print("mach is draw....")
+# elif((userchoice == 1 and computerchoice == 3) or (userchoice == 2 and computerchoice == 1)
+#      or (userchoice == 3 and computerchoice == 2)):
+#     print("you win....")
+# else:
+#     print("computer win...")
+
