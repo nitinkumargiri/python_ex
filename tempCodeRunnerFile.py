cd7fca418f9cb8fcd9b59dcb172f67b7fd5988fa
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-22 09:24:44 +0530

    tru

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,4 @@
-guess = int (input("enter your number : "))
-# import random
+import random
 # computerguess = random.randrange(1,100)
 # print("computer guess no is : ",computerguess)
 # if(guess < computerguess):
