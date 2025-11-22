fabab5ed00309348b1a8068d52306b9a0aa0dfe3
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-22 09:51:42 +0530

    calendar

--- generate_calendar.py
+++ generate_calendar.py
@@ -1,7 +1,11 @@
 print("        CALENDAR     ")
 
 import calendar
-yy = 2021
-mm = 12
-#display the calendar
-print(calendar.month(yy , mm,))
+# calensar of january 2025 
+yy = 2025
+mm = 1
+print(calendar.month(yy,mm))
+# print the calendar of febuary 2025
+yy = 2025
+mm = 2
+print(calendar.month(yy,mm))
