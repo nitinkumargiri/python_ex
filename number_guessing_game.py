47213a3d1fc5f4997db84dd47856562eb8f53e30
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-22 09:25:54 +0530

    yreb

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,7 +1,7 @@
 guess = int (input("enter your number : "))
-# import random
-# computerguess = random.randrange(1,100)
-# print("computer guess no is : ",computerguess)
+import random
+computerguess = random.randrange(1,100)
+print("computer guess no is : ",computerguess)
 # if(guess < computerguess):
 #     print("number too low....")
 # elif(guess > computerguess):
