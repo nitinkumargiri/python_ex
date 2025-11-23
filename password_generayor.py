944cb13eeecd8cabd356ff42fbf978c2f78c5e0e
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-23 14:33:08 +0530

    password_generator

--- password_generayor.py
+++ password_generayor.py
@@ -3,7 +3,7 @@ print("===password generater===\n")
 import string
 character = "abcdefghijklmnoppqrstuvwxyzABCEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
 length = int (input("enter your number : "))
-# import random
-# password = "".join(random.choice(character)for _ in range(length))
-# print ("\nyour generated password is : ",password)
+import random
+password = "".join(random.choice(character)for _ in range(length))
+print ("\nyour generated password is : ",password)
 
