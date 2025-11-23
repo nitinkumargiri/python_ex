981bf00d8faf989532e09f47cf8b492904d577ab
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-23 14:30:55 +0530

    num

---
+++ find_largest_no.py
@@ -0,0 +1,11 @@
+# num1 = int(input("enter your number : "))
+# num2 = int(input("enter your second number : "))
+# num3 = int(input("enter your third number : "))
+# if(num1 > num2 and num1 > num3):
+#     print("the gretest number is : ",num1)
+# elif(num2 > num1 and num2 > num3):
+#     print("the highest number is : ",num2)
+# elif(num3 > num1 and num3 > num2):
+#     print("your gretest number is : ",num3)
+# else:
+#     print("All number is same .....")
---
+++ password_generayor.py
@@ -0,0 +1,9 @@
+print ("...WELCOME TO PASSWORD GENERATOR GAME...\n")
+print("===password generater===\n")
+# import string
+# character = "abcdefghijklmnoppqrstuvwxyzABCEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
+# length = int (input("enter your number : "))
+# import random
+# password = "".join(random.choice(character)for _ in range(length))
+# print ("\nyour generated password is : ",password)
+
