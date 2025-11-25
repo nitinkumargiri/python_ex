7645e2d0cd761aa141f42f0327d4349fab8fa42e
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-25 12:55:28 +0530

    area_of_rectangle

--- find_area_of_rectangle.py
+++ find_area_of_rectangle.py
@@ -1,1 +1,4 @@
-area = int (input("enter your length : "))
+length = int (input("enter your length : "))
+width = int (input ("enter your width : "))
+area = length * width
+print(f"the area of \nlength = {length}\nwidth = {width}\narea is = ",area)
