2a0d15e5b21489cb726d4591f3e044f5c8b2d513
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-26 20:44:14 +0530

    give_ reverse_number

--- reverse.py
+++ reverse.py
@@ -1,9 +1,9 @@
-print("====WELCOME====")
-num1 = int (input("enter your number : "))
-rev = 0
-while(num1 > 0):
-    rev = rev * 10 + num1 %10
-    num1 = num1/10
-    break
-print("your reverse no is ",rev)
+# print("====WELCOME====\n")
+# num1 = int (input("enter your number : "))
+# rev = 0
+# while(num1 > 0):
+#     rev = rev * 10 + num1 %10
+#     num1 = num1/10
+#     break
+# print("your reverse no is ",rev)
      
