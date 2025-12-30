6a81dfedf9e6077796c10353e860ee10318ecf7a
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-12-30 11:05:40 +0530

    temperature

--- program/find_data_of_user.py
+++ program/find_data_of_user.py
@@ -1,8 +1,12 @@
 my_dict = {
     "nitin":20241094,
-     'abhi':20241111
+     'abhi':2024111,
+     'rahul':123456765,
+     'sinu':7346647,
 }
 user = input("enter user info : ")
 if user in my_dict:
     print("phone",my_dict[user])
+else:
+    print('invalid user input')
    
---
+++ program/tempratre_mesurement.py
@@ -0,0 +1,10 @@
+temp = int(input("Entyer your temperature: "))
+#mesure temperature.
+if temp >= 30:
+    print("It's a hot outside...")
+elif temp >= 20:
+    print("It's warm outside...")
+elif temp >= 10:
+    print("it's cooled outside..")
+else:
+    print("it's cool outside..")
