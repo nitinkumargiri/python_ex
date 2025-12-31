724c9b883a7be76b388b0a45786b5c123c3b96b8
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-12-31 15:56:43 +0530

    ge_per

--- program/get_result_persentage.py
+++ program/get_result_persentage.py
@@ -13,7 +13,24 @@ hindi = int(input("enter your hindi result: "))
 eng = int(input("enter your english result: "))
 math = int(input("enter your math result: "))
 science = int (input("enter your science result: "))
-sst = int(input(input("enter your social science result:")))
+sst = int(input("enter your social science result:"))
 
 total_mark = hindi + eng + math + science + sst
-percentage = (total_mark/500) + 100
+percentage = int(total_mark / 5)
+
+if percentage >= 80:
+    grade = 'A+'
+elif percentage >= 70:
+    grade = 'A'
+elif percentage >= 60:
+    grade = 'B+'
+elif percentage >= 50:
+    grade = 'B'
+elif percentage >= 30:
+    grade = 'C'
+else:
+    grade = 'F'
+
+print(f"your total mark is : {total_mark}")    
+print(f"your percentage is : {percentage}%")
+print("your grad is : ",grade)
