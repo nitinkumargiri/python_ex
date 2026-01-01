73139568245cb20feddccdf1c977c6a897ea3416
nitinkumargiri <nitingirinitingiri240@gmail.com>
2026-01-01 16:04:44 +0530

    list

---
+++ program/list_storint.py
@@ -0,0 +1,11 @@
+item = []
+
+#take list input from user
+n = input ("enter number of item you want to add in list: ")
+
+#by using for loop
+for i in range(n):
+    value = input(f"enter item : ")
+
+    print("stored list: ",item)
+    
