38208cefb21589d646f2f4018441e8151861bf5e
nitinkumargiri <nitingirinitingiri240@gmail.com>
2026-01-04 15:22:08 +0530

    find_how_many_vowel_vonsconent_in_string

--- program/calculato_using_function.py
+++ program/calculato_using_function.py
@@ -1,3 +1,14 @@
+def my_func():
+    print("welcome")
+my_func()
+my_func()
+def fav_food (food):
+    print(f"nitin fav food is {food}")
+fav_food("pizza")
+fav_food("burger")
+fav_food("samosa")
 
-
-   
+def multi(a, b):
+    return (a*b)
+multi(7, 4)
+multi(2, 8)
---
+++ simple_pro/vowel_cons.py
@@ -0,0 +1,16 @@
+def vowconst(user_input):
+    #define vowel
+    vowel = "aeiouAEIOU"
+    vowelcount = 0
+    conscount = 0
+    for eachchar in user_input:
+        if eachchar.isalpha():
+            if eachchar in vowel:
+                vowelcount += 1
+            else:
+                conscount += 1
+    return vowelcount, conscount
+user_input = input("Enter a string: ")
+vowel, conscount = vowconst(user_input)
+print(f" vowel is :{vowel}\n conscount is :{conscount}")
+            
